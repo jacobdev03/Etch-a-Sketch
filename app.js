2143965013b6ca37e542ae10920103e74297578e
jacobdev03 <dev.jacob03@gmail.com>
2021-02-18 20:06:08 +0100

    added 16x16 grid with hover effect on each square

--- app.js
+++ app.js
@@ -2,7 +2,29 @@ const mainDiv = document.querySelector('.grid-container')
 let div = document.createElement('div')
 div.classList.add('div-el')
 
+
 for(let i = 1; i <= 256; i++) {
     mainDiv.appendChild(div.cloneNode(true));
-
 }
+let divs = document.querySelectorAll('.grid-container > div')
+console.log(divs)
+
+divs.forEach(el => {
+    el.addEventListener('mouseover', () => {
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        el.style.backgroundColor = "#" + randomColor;
+    })
+    
+});
+
+// divs.addEventListener('mouseover', () => {
+//     divs.style.backgroundColor = 'lime';
+// })
+
+// mainDiv.addEventListener('mouseover', () => {
+//     mainDiv.style.backgroundColor = 'blue';
+// })
+
+// mainDiv.addEventListener('mouseout', () => {
+//     mainDiv.style.backgroundColor = 'white';
+// })
