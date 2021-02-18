6d39ecc6ac45110551240898e86711e05a12e446
jacobdev03 <dev.jacob03@gmail.com>
2021-02-18 22:52:06 +0100

    added function that generates grid in  size that user input

--- app.js
+++ app.js
@@ -1,30 +1,49 @@
-const mainDiv = document.querySelector('.grid-container')
-let div = document.createElement('div')
-div.classList.add('div-el')
+const mainDiv = document.querySelector(".grid-container");
+let div = document.createElement("div");
+const reset = document.querySelector(".reset");
+div.classList.add("div-el");
 
+mainDiv.style.gridTemplateColumns = "repeat(16, 1fr)";
+mainDiv.style.gridTemplateRows = "repeat(16, 1fr)";
 
-for(let i = 1; i <= 256; i++) {
-    mainDiv.appendChild(div.cloneNode(true));
+for (let i = 1; i <= 256; i++) {
+  mainDiv.appendChild(div.cloneNode(true));
 }
-let divs = document.querySelectorAll('.grid-container > div')
-console.log(divs)
 
-divs.forEach(el => {
-    el.addEventListener('mouseover', () => {
-        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        el.style.backgroundColor = "#" + randomColor;
-    })
-    
+hover();
+
+reset.addEventListener("click", () => {
+  resetGrid();
 });
 
-// divs.addEventListener('mouseover', () => {
-//     divs.style.backgroundColor = 'lime';
-// })
+function resetGrid() {
+  divs.forEach(div => {
+    div.style.backgroundColor = "white";
+  });
 
-// mainDiv.addEventListener('mouseover', () => {
-//     mainDiv.style.backgroundColor = 'blue';
-// })
+  do {
+    var size = prompt("Enter new size from 1 - 64");
+    parseInt(size);
+  } while (size > 64 || !size);
 
-// mainDiv.addEventListener('mouseout', () => {
-//     mainDiv.style.backgroundColor = 'white';
-// })
+  if (size <= 64) {
+    mainDiv.innerHTML = ""; // clear mainDiv from all childs
+    mainDiv.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    mainDiv.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    size *= size;
+    for (let i = 1; i <= size; i++) {
+      mainDiv.appendChild(div.cloneNode(true));
+    }
+    hover();
+  }
+}
+
+function hover() {
+  divs = document.querySelectorAll(".grid-container > div");
+  divs.forEach(el => {
+    el.addEventListener("mouseover", () => {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      el.style.backgroundColor = "#" + randomColor;
+    });
+  });
+}
